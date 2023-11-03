98c18d9a221f9b994659867513ff85fc6579de36
Nick Whitelegg <nickw4426@gmail.com>
2023-11-03 15:23:24 +0100

    Add matrix axis rotation method

--- app/src/main/java/freemap/openglwrapper/GLMatrix.kt
+++ app/src/main/java/freemap/openglwrapper/GLMatrix.kt
@@ -4,6 +4,7 @@ import android.opengl.Matrix
 
 class GLMatrix {
     val values = FloatArray(16)
+    val axes = mapOf('x' to floatArrayOf(1.0f, 0.0f, 0.0f), 'y' to floatArrayOf(0.0f, 1.0f, 0.0f), 'z' to floatArrayOf(0.0f, 0.0f, 1.0f))
 
     init {
         setAsIdentityMatrix()
@@ -23,5 +24,17 @@ class GLMatrix {
 
     fun rotate(angle: Float, x: Float, y: Float, z: Float) {
         Matrix.rotateM(values, 0, angle, x, y, z)
+    }
+
+    fun rotateAboutAxis(angle: Float, axis: Char) : Boolean {
+        axes[axis]?.apply {
+            rotate(angle, this[0], this[1], this[2])
+            return true
+        }
+        return false
+    }
+
+    fun rotateAboutY(angle: Float) {
+        rotate(angle, 1.0f, 0.0f, 0.0f)
     }
 }
